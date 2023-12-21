ba4b547c1471d07ee32f87a9d9d1887e6d8304ba
ArianEVM <109427302+theariansalmanzadeh@users.noreply.github.com>
2023-12-21 20:46:16 +0330

    update : wokring on env variables for deployment

--- minter/vite.config.ts
+++ minter/vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const env = loadEnv("all", process.cwd());
+
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
     port: 8080,
+  },
+  define: {
+    VITE_WC_PORJECT_ID: JSON.stringify(env.VITE_WC_PORJECT_ID),
+
+    VITE_PORJECT_ID: JSON.stringify(env.VITE_PORJECT_ID),
+
+    VITE_CLIENT_KEY: JSON.stringify(env.VITE_CLIENT_KEY),
+    VITE_APP_ID: JSON.stringify(env.VITE_APP_ID),
   },
 });
