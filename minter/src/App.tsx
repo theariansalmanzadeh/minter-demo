3d941c69dcd454b16c4956f2936628c31c3f5fae
ArianEVM <109427302+theariansalmanzadeh@users.noreply.github.com>
2024-01-23 00:49:21 +0330

    add : added metadata to html file

--- minter/src/App.tsx
+++ minter/src/App.tsx
@@ -1,11 +1,29 @@
+import { useLayoutEffect, useState } from "react";
 // import "./App.css";
 import "@particle-network/connectkit/dist/index.css";
+
 import Panel from "@/components/Panel/Panel";
+import { checkIsPhone } from "@/utils/global";
+// import Banner from "./components/BannerPhone/Banner";
 
 function App() {
+  const [bannerPhone, setBannerPhone] = useState<JSX.Element | null>(null);
+
+  const getBannerForPhone = async () => {
+    const module = await import("./components/BannerPhone/Banner");
+    setBannerPhone(<module.default />);
+  };
+
+  useLayoutEffect(() => {
+    if (checkIsPhone()) {
+      getBannerForPhone();
+    }
+  }, []);
+
   return (
     <div className="w-full h-full">
       <Panel />
+      {checkIsPhone() && bannerPhone}
     </div>
   );
 }
